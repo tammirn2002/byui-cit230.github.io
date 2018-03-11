f08cc0a9054a4cce7671da11dea8ef646dd907ee
tammirn2002 <35283447+tammirn2002@users.noreply.github.com>
2018-03-10 19:33:52 -0500

    4

--- assignments/lesson-9/scripts/wuweather-f.js
+++ assignments/lesson-9/scripts/wuweather-f.js
@@ -1,12 +1,26 @@
-var section = document.querySelector('section');
+var requestURL = 'http://api.wunderground.com/api/ffbb53a904bff0c1/conditions/q/MN/Franklin.json';
 
-var requestURL = 'http://api.wunderground.com/api/ffbb53a904bff0c1/conditions/q/MN/Franklin.json'
 var currentcond = new XMLHttpRequest();
-currentcond.open('GET' , requestURL, true);
+currentcond.open('GET', requestURL, true);
 currentcond.send();
 
 currentcond.onload = function () {
     var fweather = JSON.parse(currentcond.responseText);
     console.log(fweather);
-    document.getElementById('currentTemp').innerHTML =
+    document.getElementById('hightemp').innerHTML = fweather.current_observation.temp_f;
+    document.getElementById('weathericon').src = fweather.current_observation.icon_url;
+    document.getElementById('currentW').innerHTML = fweather.current_observation.weather;
+    document.getElementById('wspeed').innerHTML = fweather.current_observation.wind_mph;
+    document.getElementById('windchill').innerHTML = fweather.current_observation.windchill_f;
+    document.getElementById('precip').innerHTML = fweather.current_observation.precip_1hr_in;
+}
+
+var curconditions = new XMLHttpRequest();
+curconditions.open("GET", "http://api.wunderground.com/api/ffbb53a904bff0c1/forecast/q/MN/Franklin/json", true);
+curconditions.send();
+
+curconditions.onload = function () {
+    var text = JSON.parse(curconditions.responseText);
+    console.log(text);
+    document.getElementById('weathercond').innerHTML = text.forecast.txt_forecast.forecastday["0"].fcttext;
 }
