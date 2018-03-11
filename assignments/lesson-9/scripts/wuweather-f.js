dabeee3c298dddf1df53aaed1f80c63bdff60218
tammirn2002 <35283447+tammirn2002@users.noreply.github.com>
2018-03-11 16:47:00 -0400

    2

--- assignments/lesson-9/scripts/wuweather-f.js
+++ assignments/lesson-9/scripts/wuweather-f.js
@@ -7,10 +7,11 @@ currentcond.send();
 currentcond.onload = function () {
     var fweather = JSON.parse(currentcond.responseText);
     console.log(fweather);
-    document.getElementById('hightemp').innerHTML = fweather.current_observation.temp_f;
+    document.getElementById('curtemp').innerHTML = fweather.current_observation.temp_f;
     document.getElementById('weathericon').src = fweather.current_observation.icon_url;
     document.getElementById('currentW').innerHTML = fweather.current_observation.weather;
     document.getElementById('wspeed').innerHTML = fweather.current_observation.wind_mph;
+    document.getElementById('wdirection').innerHTML = weatherinfo.current_observation.wind_dir;
     document.getElementById('windchill').innerHTML = fweather.current_observation.windchill_f;
     document.getElementById('precip').innerHTML = fweather.current_observation.precip_1hr_in;
 
@@ -24,7 +25,7 @@ curconditions.send();
 curconditions.onload = function () {
     var curcon = JSON.parse(curconditions.responseText);
     console.log(text);
-    document.getElementById('weathercond').innerHTML = curcon.forecast.txt_forecast.forecastday["0"].fcttext;
-    document.getElementById('lowtemp').innerHTML = curcon.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
+    document.getElementById('weatherfore').innerHTML = curcon.forecast.txt_forecast.forecastday["0"].fcttext;
+
 
 }
