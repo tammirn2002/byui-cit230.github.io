64a191184bc1091f7b5f3be45c140dfc9cf3ffbc
tammirn2002 <35283447+tammirn2002@users.noreply.github.com>
2018-03-13 17:26:53 -0400

    2

--- assignments/lesson-9/scripts/wuweather-f.js
+++ assignments/lesson-9/scripts/wuweather-f.js
@@ -1,4 +1,4 @@
-var requestURL = 'http://api.wunderground.com/api/ffbb53a904bff0c1/conditions/q/MN/Franklin.json';
+var requestURL = 'https://api.wunderground.com/api/ffbb53a904bff0c1/conditions/q/MN/Franklin.json';
 
 var currentcond = new XMLHttpRequest();
 currentcond.open('GET', requestURL, true);
@@ -8,7 +8,7 @@ currentcond.onload = function () {
     var fweather = JSON.parse(currentcond.responseText);
     console.log(fweather);
     document.getElementById('curtemp').innerHTML = fweather.current_observation.temp_f;
-    document.getElementById('weathericon').src = fweather.current_observation.icon_url;
+    document.getElementById('weathericon').src = fweather.current_observation.icon_url.replace("http", "https");
     document.getElementById('currentW').innerHTML = fweather.current_observation.weather;
     document.getElementById('wspeed').innerHTML = fweather.current_observation.wind_mph;
     document.getElementById('windchill').innerHTML = fweather.current_observation.windchill_f;
@@ -17,14 +17,14 @@ currentcond.onload = function () {
 }
 //forecast conditions
 
-var curconditions = new XMLHttpRequest();
-curconditions.open("GET", "http://api.wunderground.com/api/ffbb53a904bff0c1/forecast/q/MN/Franklin/json", true);
-curconditions.send();
+var requestfore = 'http://api.wunderground.com/api/ffbb53a904bff0c1/forecast/q/mn/Franklin.json';
 
-curconditions.onload = function () {
-    var curcon = JSON.parse(curconditions.responseText);
-    console.log(text);
+var forecon = new XMLHttpRequest();
+forecon.open('GET', requestfore, true);
+forecon.send();
+
+forecon.onload = function () {
+    var curcon = JSON.parse(forecon.responseText);
+    console.log(curcon);
     document.getElementById('weatherfore').innerHTML = curcon.forecast.txt_forecast.forecastday["0"].fcttext;
-
-
 }
