b173e702a3f3d233a27b5a988daa15c7a536274c
tammirn2002 <35283447+tammirn2002@users.noreply.github.com>
2018-03-16 23:36:43 -0400

    f

--- assignments/weathersite/scripts/index.js
+++ assignments/weathersite/scripts/index.js
@@ -1,7 +1,5 @@
 //Home page city info
-var franklin = document.querySelector('div.franklin');
-var greenville = document.querySelector('div.greenville');
-var springfield = document.querySelector('div.springfield');
+var section = document.querySelector('section');
 
 var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 var request = new XMLHttpRequest();
@@ -15,41 +13,36 @@ request.onload = function () {
 
 function showCities(jsonObj) {
     var city = jsonObj['towns'];
-    for (var i = 0; i < city.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH1 = document.createElement('h1');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
-        myH1.textContent = city[i].name;
-        myH2.textContent = 'Motto: ' + city[i].motto;
-        myPara1.textContent = 'Year Founded: ' + city[i].yearFounded;
-        myPara2.textContent = 'Current Population: ' + city[i].currentPopulation;
-        myPara3.textContent = 'Average Rainfall: ' + city[i].averageRainfall;
-        myList.textContent = 'Event: ';
+    for (var i = 0; i <city.length; i++) {
+        if (city[i].name == "Franklin" || city[i].name == "Springfield" || city[i].name == "Greenville") {
+                var myArticle = document.createElement(city[i].name);
+                var myH1 = document.createElement('h1');
+                var myH2 = document.createElement('h2');
+                var myPara1 = document.createElement('p');
+                var myPara2 = document.createElement('p');
+                var myPara3 = document.createElement('p');
+                var myList = document.createElement('ul');
+                myH1.textContent = city[i].name;
+                myH2.textContent = city[i].motto;
+                myPara1.textContent = 'Year Founded: ' + city[i].yearFounded;
+                myPara2.textContent = 'Current Population: ' + city[i].currentPopulation;
+                myPara3.textContent = 'Average Rainfall: ' + city[i].averageRainfall;
+                myList.textContent = 'Events: ';
 
-        var events = city[i].events;
-        for (var j = 0; j < events.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = events[j];
-            myList.appendChild(listItem);
-        }
-        myArticle.appendChild(myH1);
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
-        section.appendChild(myArticle);
-        if (city[i].name == 'Franklin') {
-            franklin.appendChild(myArticle);
-        } else if (city[i].name == 'Greenville') {
-            greenville.appendChild(myArticle);
-        } else if (city[i].name == 'Springfield') {
-            springfield.appendChild(myArticle);
+                var events = city[i].events;
+                for (var j = 0; j < events.length; j++) {
+                    var listItem = document.createElement('li');
+                    listItem.textContent = events[j];
+                    myList.appendChild(listItem);
+                }
+                myArticle.appendChild(myH1);
+                myArticle.appendChild(myH2);
+                myArticle.appendChild(myPara1);
+                myArticle.appendChild(myPara2);
+                myArticle.appendChild(myPara3);
+                myArticle.appendChild(myList);
+                section.appendChild(myArticle);
 
+            }
         }
     }
-}
